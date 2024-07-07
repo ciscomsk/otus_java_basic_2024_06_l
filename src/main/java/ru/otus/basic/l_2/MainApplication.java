bc80b7765b455919d6cbb9019b868afec4089a93
ciscomsk <kuznetzoff@gmail.com>
2024-07-07 21:28:30 +0300

    Added l_2 drawSquare

--- src/main/java/ru/otus/basic/l_2/MainApplication.java
+++ src/main/java/ru/otus/basic/l_2/MainApplication.java
@@ -2,6 +2,14 @@ package ru.otus.basic.l_2;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+        drawSquare();
+    }
+
+    public static void drawSquare() {
+        System.out.println("----");
+        System.out.println("-  -");
+        System.out.println("-  -");
+        System.out.println("----");
     }
 }
