eecc0d61bb982bae11f8c6f76d919e9c71293873
ciscomsk <kuznetzoff@gmail.com>
2024-07-08 22:00:20 +0300

    Added feature/2 l_2 feature2Method

--- src/main/java/ru/otus/basic/l_2/MainApplication.java
+++ src/main/java/ru/otus/basic/l_2/MainApplication.java
@@ -4,6 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
         drawSquare();
+        feature2Method();
     }
 
     public static void drawSquare() {
